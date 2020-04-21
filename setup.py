a33e59e6d2e3163717570a39cb37081c13319908
Miyabi <ksrgmiyabi@gmail.com>
2020-04-21 20:15:51 +0900

    test

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pstuvwx/squib",
-    packages=setuptools.find_packages(),
+    packages=['squib'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
